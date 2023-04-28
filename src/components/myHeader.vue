f0682fd4d8dcf0ac94ccf9b07ad0d8ad5a3060a4
paulmarniquet <paul.marniquet@epitech.eu>
2023-04-28 11:14:14 +0200

    Started header and title part

---
+++ src/components/myHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+        <div class="initials">
+            <p>z</p>
+            <p>&</p>
+            <p>t</p>
+        </div>
+        <div class="date">
+            <p>10</p>
+            <p>/</p>
+            <p>12</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+    font-size: 20px;
+    text-align: center;
+}
+
+.header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 40%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.date {
+    position: relative;
+    top: 0;
+    right: 0;
+    margin: 5%;
+}
+
+.initials {
+    position: relative;
+    top: 0;
+    left: 0;
+    margin: 5%;
+}
+
+</style>
---
+++ src/components/myTitle.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="title">
+        <h1>marry wednesday.</h1>
+    </div>
+    <div class="circles">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myTitle'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.title {
+    position: absolute;
+    top: 60%;
+    left: 55%;
+    width: 50%;
+    height: auto;
+    transform: translate(-50%, -50%);
+}
+
+h1 {
+    position: relative;
+    font-size: 110px;
+    text-align: left;
+}
+
+.circles {
+    position: relative;
+    justify-content: center;
+    width: 100%;
+    height: auto;
+    background: aquamarine;
+    display: flex;
+}
+
+.circle {
+    width: 20px;
+    height: 20px;
+    border: 1px solid black;
+    border-radius: 50%;
+}
+
+</style>
